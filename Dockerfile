ce16a09dd47b551db60f7079ee5c52d0f8942676
Kevin Dukelow <Kevin_Dukelow@colpal.com>
2019-11-22 17:43:20 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM google/cloud-sdk:alpine
+
+LABEL name="gcr-buildpush"
+LABEL version="1.0.0"
+LABEL com.github.actions.name="GCR Build and Push"
+LABEL com.github.actions.description="GitHub action with to build a docker container and push it to GCR"
+LABEL com.github.actions.color="blue"
+LABEL com.github.actions.icon="cloud"
+
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -l
+
+IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PPOJECT/$INPUT_GCR_REPO$INPUT_GCR_IMAGE_NAME"
+
+echo "$INPUT_GCR_SERVICE_ACCOUNT" | base64 -d > /tmp/service_account.json
+
+gcloud auth activate-service-account --key-file=/tmp/service_account.json
+
+gcloud config set project "$INPUT_GCR_PROJECT"
+
+docker build -t $IMAGE_NAME:$INPUT_IMAGE_TAG --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg GITHUB_REF="$GITHUB_REF" $INPUT_DOCKERFILE_PATH
+
+docker push $IMAGE_NAME:$INPUT_IMAGE_TAG
+gcloud container images add-tag $IMAGE_NAME:$INPUT_IMAGE_TAG $IMAGE_NAME::latest
