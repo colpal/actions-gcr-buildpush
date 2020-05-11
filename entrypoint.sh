87826531583fe93aa6f97414499fed78d529a9fe
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 11:03:42 -0400

    final message

--- entrypoint.sh
+++ entrypoint.sh
@@ -280,7 +280,6 @@ INPUT_REGISTRY=$INPUT_GCR_HOST
 INPUT_IMAGE_TAGS=$INPUT_IMAGE_TAG
 INPUT_CONTEXT=$INPUT_DOCKERFILE_PATH
 fi
-# echo "Using new code."
 init_variables
 check_required_input
 login_to_registry
@@ -290,11 +289,6 @@ version_number
 tag_image
 push_image_and_stages
 logout_from_registry
-# IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT/$INPUT_GCR_REPO$INPUT_GCR_IMAGE_NAME"
-# echo "$INPUT_GCR_SERVICE_ACCOUNT" | base64 -d > /tmp/service_account.json
-# gcloud auth activate-service-account --key-file=/tmp/service_account.json
-# gcloud config set project $INPUT_GCR_PROJECT
-# gcloud auth configure-docker
-# docker build -t $IMAGE_NAME:$INPUT_IMAGE_TAG --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg GITHUB_REF="$GITHUB_REF" $INPUT_DOCKERFILE_PATH
-# docker push $IMAGE_NAME:$INPUT_IMAGE_TAG
-# gcloud container images add-tag $IMAGE_NAME:$INPUT_IMAGE_TAG $IMAGE_NAME:latest
+if [ ! -z "$INPUT_GCR_SERVICE_ACCOUNT" ] ;then
+echo "Used deprecated code. Please switch to new parameters."
+fi
