49fa124263be7a308f747ad312caa8f1b522330e
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 12:18:00 -0400

    [PATCH]. Added message.

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MINOR\]")" ] ;th
 echo "MINOR"
 elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[PATCH\]")" ] ;then
 echo "PATCH"
+else
+echo "No version update detected."
 fi
