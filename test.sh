398e8dbf7eb5c4967f47466b5f64d2c7940cd6ab
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 12:14:24 -0400

    What about [MINOR] multiline?

--- test.sh
+++ test.sh
@@ -1,3 +1,7 @@
 if [ $(git log -1 --pretty=%B | grep -e "^\[MAJOR\]") ] ;then
 echo "MAJOR"
+elif [ $(git log -1 --pretty=%B | grep -e "^\[MINOR\]") ] ;then
+echo "MINOR"
+if [ $(git log -1 --pretty=%B | grep -e "^\[PATCH\]") ] ;then
+echo "PATCH"
 fi
