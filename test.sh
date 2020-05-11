bf9b9fa2dafe7c4e7ba89cfba34da4544183fa81
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 12:21:25 -0400

    should fail

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-if [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MAJOR\]")" ] ;then
+if [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MAJOR\]" || true)" ] ;then
 echo "MAJOR"
-elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MINOR\]")" ] ;then
+elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MINOR\]" || true)" ] ;then
 echo "MINOR"
-elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[PATCH\]")" ] ;then
+elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[PATCH\]" || true)" ] ;then
 echo "PATCH"
 else
 echo "No version update detected."
