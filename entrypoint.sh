aafab47c4fe24d4358e8ff5388e7117eb41df3f5
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 10:06:02 -0400

    grep -Eo

--- entrypoint.sh
+++ entrypoint.sh
@@ -251,7 +251,7 @@ logout_from_registry() {
 version_number(){
   if [ ! -z "$INPUT_VERSION_UPDATE_TYPE" ] ;then
     echo "zero"
-    docker pull --all-tags "$(_get_full_image_name)" | egrep -o "v[0-9]+\.[0-9]+\.[0-9]+"
+    docker pull --all-tags "$(_get_full_image_name)" | grep -Eo "v[0-9]+\.[0-9]+\.[0-9]+"
     echo $?
     echo "1"
     maxstage=$(docker pull --all-tags "$(_get_full_image_name)" | egrep -o "v[0-9]+\.[0-9]+\.[0-9]+" | egrep -o "[0-9]+\.[0-9]+\.[0-9]+" | sort -n | tail -n 1)
