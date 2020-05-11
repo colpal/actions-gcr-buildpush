1f909b216f70639daeff121606f1561427c22a88
Alexander Saff <alexander_saff@colpal.com>
2020-05-11 12:16:49 -0400

    add head [MAJOR]

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-if [ $(git log -1 --pretty=%B | grep -e "^\[MAJOR\]") ] ;then
+if [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MAJOR\]")" ] ;then
 echo "MAJOR"
-elif [ $(git log -1 --pretty=%B | grep -e "^\[MINOR\]") ] ;then
+elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[MINOR\]")" ] ;then
 echo "MINOR"
-if [ $(git log -1 --pretty=%B | grep -e "^\[PATCH\]") ] ;then
+elif [ ! -z "$(git log -1 --pretty=%B | head -n 1 | grep -e "^\[PATCH\]")" ] ;then
 echo "PATCH"
 fi
